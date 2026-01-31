673619ff7feb02c466d22d17153fe161b32efcbd
Simon Jarda <snjarda@gmail.com>
2026-01-31 21:50:37 +0100

    port change

--- server.js
+++ server.js
@@ -65,5 +65,5 @@ app.post("/api/deleteMessage", async (req, res) => {
     }
 })
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server läuft auf http://localhost:${PORT}`));
